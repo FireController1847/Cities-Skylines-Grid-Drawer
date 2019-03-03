c7f923603577eb80937d06a3a8b7081f4a7a3318
FireController1847 <usfirepilot123@gmail.com>
2019-03-02 21:20:57 -0700

    Add Skeleton For New Mod

---
+++ GridDrawer/GridDrawer/GridDrawer.cs
@@ -0,0 +1,22 @@
+﻿using ICities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace GridDrawer {
+    public class GridDrawer : IUserMod {
+        public string Name => "Grid Drawer";
+
+        public string Description => "Draws the VehicleManager Grid (m_vehicleGrid).";
+
+        public void OnEnabled() {
+            Log.Info($"Grid Drawer enabled. Build {Assembly.GetExecutingAssembly().GetName().Version}");
+        }
+
+        public void OnDisabled() {
+            Log.Info($"Grid Drawer disabled.");
+        }
+    }
+}
